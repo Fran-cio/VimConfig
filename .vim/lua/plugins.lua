c67e41f698fbe4631c512fb79e3bd8c9548cbbe0
Franciso <gatupancho@gmail.com>
2021-11-20 17:58:39 -0300

    .

---
+++ .vim/lua/plugins.lua
@@ -0,0 +1,10 @@
+return require('packer').startup(function()
+
+	--tree
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = 'kyazdani42/nvim-web-devicons',
+		config = function() require'nvim-tree'.setup {} end
+	}
+
+end)
